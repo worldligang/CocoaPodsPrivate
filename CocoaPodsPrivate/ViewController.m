54f03466f284b1ed7fccbba93616610b65df210d
ligang <li.gang@xiaoyi.com>
2015-05-08 09:19:12 +0800

    test 1

---
+++ CocoaPodsPrivate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CocoaPodsPrivate
+//
+//  Created by ligang on 15/5/8.
+//  Copyright (c) 2015年 ligang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
